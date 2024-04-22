e08cd24d6bf845b5d065f53d5de3d33ac64938de
Nazareno <apicellanazareno1@gmail.com>
2024-04-22 17:07:09 -0300

    Hace programa de area cuadrado

--- main.c
+++ main.c
@@ -2,6 +2,16 @@
 
 int main(){
 
+float lado1, lado2, area;
+	printf("Ingrese su alto en cm: ");
+	scanf("%f", &lado1);
+	printf("Ingrese su ancho en cm: ");
+        scanf("%f", &lado2);
+
+area= lado1 * lado2;
+
+	printf("su area en cm² es: %g/n", area);
+        
 	return 0;
 
 
