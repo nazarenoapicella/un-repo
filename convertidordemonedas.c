30d0ca44051c3590dc8834cefcfc7070eac6aca1
Nazareno <apicellanazareno1@gmail.com>
2024-04-22 20:34:15 -0300

    Convertir ARS al actual cambio de USD blue

--- convertidordemonedas.c
+++ convertidordemonedas.c
@@ -7,7 +7,7 @@ float pesos, dolares;
 	printf("Ingrese el monto en ARS: ");
 	scanf("%f", &pesos);
 
-dolares= pesos/1000;
+dolares = pesos/1000;
 
 	printf("Su cantidad en USD Blue es: %g\n", dolares);
 
