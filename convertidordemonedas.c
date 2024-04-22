c4fdec8d05d5c7e7269ed68c66541cc98dc5086b
Nazareno <apicellanazareno1@gmail.com>
2024-04-22 20:26:36 -0300

    Calculadora de USD al actual cambio argentino

---
+++ convertidordemonedas.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+float pesos, dolares;
+
+	printf("Ingrese el monto en ARS: ");
+	scanf("%f", &pesos);
+
+dolares= pesos/1000;
+
+	printf("Su cantidad en USD Blue es: %g\n", dolares);
+
+return 0;
+
+}
