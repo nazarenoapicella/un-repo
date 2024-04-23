680da4d43dded58cd5d4f48e7cad46e1f436d1b1
Nazareno <apicellanazareno1@gmail.com>
2024-04-23 16:10:31 -0300

    calcu terminada

--- calculadora.c
+++ calculadora.c
@@ -1,24 +1,53 @@
 #include <stdio.h>
-
+#include <math.h>
 
 int main(){
 
 	float numero1, numero2, resultado;
 	char operacion;
 
-	printf("Ingrese la operacion: ");
+	printf("Ingrese la operacion (+, -, *, /, ), ^): ");
 	scanf("%c", &operacion);
 	printf("Ingrese el numero 1: ");
         scanf("%f", &numero1);
-	printf("Ingrese el numero 2: ");                                                                                         scanf("%f", &numero2);
+	printf("Ingrese el numero 2: ");                                                                                         
+	scanf("%f", &numero2);
 
 if (operacion == '+') {
 	resultado = numero1 + numero2;
 	printf("El resultado es: %g\n", resultado);
 
 }
-
-return 0;
+if (operacion == '-') {
+        resultado = numero1 - numero2;
+        printf("El resultado es: %g\n", resultado);
 
 }
 
+if (operacion == '*') {
+        resultado = numero1 * numero2;
+        printf("El resultado es: %g\n", resultado);
+
+}
+if (operacion == '/') {  
+        resultado = numero1 / numero2;
+        printf("El resultado es: %g\n", resultado);
+
+}
+
+       else if (operacion == '^') {
+        // Calcular el exponente multiplicando la base consigo misma
+        resultado = 1;
+        for (int i = 0; i < numero2; i++) {
+            resultado *= numero1;
+        }
+    }
+    else {
+        printf("Operacion no valida.\n");
+        return 1; // Salir del programa con error
+    }
+
+    printf("El resultado es: %g\n", resultado);
+
+    return 0;
+}
