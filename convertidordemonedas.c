8e19cfa5e004bdbaaa1faa93c2b0c4c804225b4c
Nazareno <apicellanazareno1@gmail.com>
2024-04-24 21:48:46 -0300

    calculadora mejorada

--- convertidordemonedas.c
+++ convertidordemonedas.c
@@ -1,16 +1,35 @@
 #include <stdio.h>
 
-int main(){
+int main() {
+    float dolares = 0, euros = 0, pesos = 0;
+    int divisa;
 
-float pesos, dolares;
+    printf("Ingrese la divisa (pesos = 1, USD = 2, y euros = 3): ");
+    scanf("%d", &divisa);
 
-	printf("Ingrese el monto en ARS: ");
-	scanf("%f", &pesos);
+    if (divisa == 1) {
+        printf("Ingrese la cantidad en pesos: ");
+        scanf("%f", &pesos);
+        
+        dolares = pesos / 1000;
+        euros = pesos / 934;
+    } else if (divisa == 2) {
+        printf("Ingrese la cantidad en USD: ");
+        scanf("%f", &dolares);
+        
+        pesos = dolares * 1000;
+        euros = pesos / 934;
+    } else if (divisa == 3) {
+        printf("Ingrese la cantidad en euros: ");
+        scanf("%f", &euros);
+        
+        dolares = euros * 1.07;
+        pesos = euros * 934;
+    }
 
-dolares = pesos/1000;
+    printf("Su cantidad en dólares es: %g\n", dolares);
+    printf("Su cantidad en pesos es: %g\n", pesos);
 
-	printf("Su cantidad en USD Blue es: %g\n", dolares);
-
-return 0;
-
+    return 0;
 }
+
