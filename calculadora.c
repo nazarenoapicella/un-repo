0323cab0d7543cd66e2455663b75a18e27f6d528
Nazareno <apicellanazareno1@gmail.com>
2024-04-28 19:58:08 -0300

    mejora calculadora.c

--- calculadora.c
+++ calculadora.c
@@ -8,43 +8,59 @@ int main() {
     printf("Ingrese la operacion (+, -, *, /, ^, r): ");
     scanf(" %c", &operacion);
 
+    if (operacion == '+') {
     printf("Ingrese el numero 1: ");
     scanf("%f", &numero1);
     printf("Ingrese el numero 2: ");
     scanf("%f", &numero2);
-
-    if (operacion == '+') {
-        resultado = numero1 + numero2;
-        printf("El resultado es: %g\n", resultado);
+    resultado = numero1 + numero2;
+    printf("El resultado es: %g\n", resultado);
     }
     else if (operacion == '-') {
-        resultado = numero1 - numero2;
-        printf("El resultado es: %g\n", resultado);
+    printf("Ingrese el numero 1: ");
+    scanf("%f", &numero1);
+    printf("Ingrese el numero 2: ");
+    scanf("%f", &numero2);
+    resultado = numero1 - numero2;
+    printf("El resultado es: %g\n", resultado);
     }
     else if (operacion == '*') {
-        resultado = numero1 * numero2;
-        printf("El resultado es: %g\n", resultado);
+    printf("Ingrese el numero 1: ");
+    scanf("%f", &numero1);
+    printf("Ingrese el numero 2: ");
+    scanf("%f", &numero2);
+    resultado = numero1 * numero2;
+    printf("El resultado es: %g\n", resultado);
     }
     else if (operacion == '/') {
-        resultado = numero1 / numero2;
-        printf("El resultado es: %g\n", resultado);
+    printf("Ingrese el numero 1: ");
+    scanf("%f", &numero1);
+    printf("Ingrese el numero 2: ");
+    scanf("%f", &numero2);
+    resultado = numero1 / numero2;
+    printf("El resultado es: %g\n", resultado);
     }
     else if (operacion == '^') {
-        resultado = 1;
-        for (int i = 0; i < numero2; i++) {
-            resultado *= numero1;
-        }
-        printf("El resultado es: %g\n", resultado);
+    printf("Ingrese el numero 1: ");
+    scanf("%f", &numero1);
+    printf("Ingrese el numero 2: ");
+    scanf("%f", &numero2);
+    resultado = 1;
+    for (int i = 0; i < numero2; i++) {
+    resultado *= numero1;
+    }
+    printf("El resultado es: %g\n", resultado);
     }
     else if (operacion == 'r') {
-        resultado = sqrt(numero1);
-        printf("El resultado es: %g\n", resultado);
+    printf("Ingrese el numero a radicar: ");
+    scanf("%f", &numero1);
+    resultado = sqrt(numero1);
+    printf("El resultado es: %g\n", resultado);
     }
     else {
-        printf("Operacion no valida.\n");
-        return 1;
+    printf("Operacion no valida.\n");
+    return 1;
     }
 
     return 0;
-}
-
+} 
